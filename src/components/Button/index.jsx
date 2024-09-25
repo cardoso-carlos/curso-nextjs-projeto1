a0071eb20a39aa7c16da63021c2c184121b4d44e
Carlos Augusto <carlos.augusto.cpd@gmail.com>
2024-09-25 09:25:43 -0300

    configuração botão load more posts

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,6 @@ import './styles.css';
 
 export const Button = ({ text, onClick, disabled }) => (
     <button 
-        type="button" 
         className="button" 
         onClick={onClick} 
         disabled={disabled}
